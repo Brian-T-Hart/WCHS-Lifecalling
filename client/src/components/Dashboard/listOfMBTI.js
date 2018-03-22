c78cce8a6f8758fad0c57c917d055f9219994375
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2018-03-22 07:32:08 -0700

    added meyers-briggs type indicators

---
+++ client/src/components/Dashboard/listOfMBTI.js
@@ -0,0 +1,44 @@
+const listOfMBTI = [
+    {
+        id: 1,
+        name: "Extrovert",
+        description: "Is energized by other people."
+    },
+    {
+        id: 2,
+        name: "Introvert",
+        description: "Is energized by yourself."
+    },
+    {
+        id: 3,
+        name: "Sensing",
+        description: "the reliance on observing facts or events through the five senses. Individuals with this preference tend to see what is happening now and what reality is."
+    },
+    {
+        id: 4,
+        name: "Intuition",
+        description: "the reliance on creating meaning, seeing relationships and possibilities when observing facts or events."
+    },
+    {
+        id: 5,
+        name: "Thinking",
+        description: "the reliance or preference of making decisions based on a logical impersonal process."
+    },
+    {
+        id: 6,
+        name: "Feeling",
+        description: "the reliance or preference to make decisions based on social values and relationships."
+    },
+    {
+        id: 7,
+        name: "Perceiving",
+        description: "that is they like to keep exploring information, and people who prefer."
+    },
+    {
+        id: 8,
+        name: "Judging",
+        description: "that is they like to bring the decision-making process to a close."
+    }
+]
+
+export default listOfMBTI;
