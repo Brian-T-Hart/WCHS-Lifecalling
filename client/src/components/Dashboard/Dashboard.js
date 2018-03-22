55ef7578e78cd660d565d661caeeabae066244c0
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2018-03-21 22:41:34 -0700

    moved list of strengths to its own file.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import MyStrengths from "../MyStrengths/MyStrengths.js";
 import ModalWrapper from '../ModalWrapper/ModalWrapper.js';
+import MBTI from "../MBTI/MBTI.js";
+import listOfStrengths from "./listOfStrengths.js";
 
 class Dashboard extends Component {
 
@@ -28,212 +30,7 @@ class Dashboard extends Component {
         this.state = {
             username: "",
             myStrengths: [],
-            strengths: [
-                {
-                    id: 1,
-                    name: "achiever",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 2,
-                    name: "activator",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 3,
-                    name: "adaptability",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 4,
-                    name: "analytical",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 5,
-                    name: "arranger",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 6,
-                    name: "belief",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 7,
-                    name: "command",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 8,
-                    name: "communication",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 9,
-                    name: "competition",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 10,
-                    name: "connectedness",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 11,
-                    name: "consistency",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 12,
-                    name: "context",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 13,
-                    name: "deliberative",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 14,
-                    name: "developer",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 15,
-                    name: "discipline",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 16,
-                    name: "empathy",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 17,
-                    name: "focus",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 18,
-                    name: "futuristic",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 19,
-                    name: "harmony",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 20,
-                    name: "ideation",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 21,
-                    name: "includer",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 22,
-                    name: "Individualization",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 23,
-                    name: "input",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 24,
-                    name: "intellection",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 25,
-                    name: "learner",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 26,
-                    name: "maximizer",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 27,
-                    name: "positivity",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 28,
-                    name: "realtor",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 29,
-                    name: "responsibility",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 30,
-                    name: "restorative",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 31,
-                    name: "self assurance",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 32,
-                    name: "significance",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 33,
-                    name: "strategic",
-                    description: "",
-                    strengthOfMine: false
-                },
-                {
-                    id: 34,
-                    name: "woo",
-                    description: "",
-                    strengthOfMine: false
-                }
-            ]
+            strengths: listOfStrengths
         }
     }
 
@@ -257,7 +54,7 @@ class Dashboard extends Component {
                                 <MyStrengths strengths={this.state.strengths} myStrengths={this.state.myStrengths}/>
                             </div>
                             <div className="infoDiv col-md-4">
-                                {/* <MyStrengths /> */}
+                                <MBTI />
                             </div>
                             <div className="infoDiv col-md-4">
                                 {/* <MyStrengths /> */}
