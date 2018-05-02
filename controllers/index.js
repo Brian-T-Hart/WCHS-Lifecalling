ee4b035883b1dc79d22d6886b4d474afd6d13aa1
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2018-05-02 14:39:39 -0700

    removed unnecessary code

--- controllers/index.js
+++ controllers/index.js
@@ -10,20 +10,4 @@ router.get('*', (req, res, next) => {
     res.sendFile(path.join(__dirname + '../../client/build/index.html'));
 });
 
-// post to get all products from a particular rep
-router.post('/testCall', (req, res, next) => {
-    console.log("_________________________________");
-    console.log("testCall..index.routes.js");
-    console.log("_________________________________");
-    db.students.findAll({
-        where: {
-            username: req.body.username
-        }
-        // order: db.sequelize.col('customer_company')
-    }).then(function (results) {
-        console.log("results from testCall..index.js ", results);
-        res.json(results)
-    });
-});
-
 module.exports = router;
