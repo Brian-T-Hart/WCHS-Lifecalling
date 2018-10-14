98f69657eb2d218f1dea0fdb2c15abc747945bd4
Brian-T-Hart <hart.briant@gmail.com>
2018-10-14 14:15:09 -0700

    I updated the look of the admin page so the student info is listed in a table.

--- client/src/components/AllStudents/AllStudents.js
+++ client/src/components/AllStudents/AllStudents.js
@@ -34,24 +34,75 @@ class AllStudents extends Component {
 	render() {
 
 		const allTheStudents = this.state.allStudents.map((student) =>
-				<div className="studentInfoContainer" key={student.id}>
-					<h5>{student.id}</h5>
-					<h5>{student.username}</h5>
-					<h5>{student.email}</h5>
-					<h5>{student.strengths}</h5>
-					<h5>{student.mbti}</h5>
-					<h5>{student.gifts}</h5>
-					<h5>{student.loveLanguages}</h5>
-					<h5>{student.intelligences}</h5>
-					<h5>{student.documentUrl}</h5>
-				</div>
+				<tr className="studentInfoRow" key={student.id}>
+					<td>{student.username}</td>
+					<td>{student.email}</td>
+
+					<td>
+						{student.strengths != null ? 
+							<ul>
+								{student.strengths.map((myStrength) => <li key={myStrength}>{myStrength}</li>)}
+							</ul> : ""}
+					</td>
+
+					<td>
+						{student.mbti != null ? 
+							<ul>
+								{student.mbti.map((type) => <li key={type}>{type}</li>)}
+							</ul> : ""}
+					</td>
+
+					<td>
+						{student.gifts != null ? 
+							<ul>
+								{student.gifts.map((gift) => <li key={gift}>{gift}</li>)}
+							</ul> : ""}
+					</td>
+
+					<td>
+						{student.loveLanguages != null ? 
+							<ul>
+								{student.loveLanguages.map((loveLanguage) => <li key={loveLanguage}>{loveLanguage}</li>)}
+							</ul> : ""}
+					</td>
+
+					<td>
+						{student.intelligences != null ? 
+							<ul>
+								{student.intelligences.map((intelligence) => <li key={intelligence}>{intelligence}</li>)}
+							</ul> : ""}
+					</td>
+
+					<td>
+						<a href={student.learningStylesUrl}>Learning Styles</a><br></br>
+						<a href={student.johariUrl}>Johari Window</a><br></br>
+						<a href={student.documentUrl}>CV</a>
+					</td>
+				</tr>
 		)
 
 		return (
 
 			<div>
 				<Navbar />
-				{ allTheStudents }
+				<table className="table all-students">
+					<thead>
+						<tr>
+						<th scope="col">Username</th>
+						<th scope="col">Email</th>
+						<th scope="col">Strengths</th>
+						<th scope="col">Myers-Briggs</th>
+						<th scope="col">Spiritual Gifts</th>
+						<th scope="col">Love Languages</th>
+						<th scope="col">Multiple-Intelligences</th>
+						<th scope="col">Documents</th>
+						</tr>
+					</thead>
+
+					<tbody>
+						{ allTheStudents }
+					</tbody>
+				</table>
 			</div>
 		)
 	}
