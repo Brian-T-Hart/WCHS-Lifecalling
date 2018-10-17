cb73d7e7880ef2a23ad6d53dbe4ee0612b76da08
Brian-T-Hart <hart.briant@gmail.com>
2018-10-16 20:45:53 -0700

    updated package.json files and git ignore

---
+++ client/build/precache-manifest.cd7cd527cf745efdb2d0b8bbeef68dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e5efcf3fbff394eded74",
+    "url": "/static/js/main.e5efcf3f.chunk.js"
+  },
+  {
+    "revision": "0499dbdb9389b2e8cd1a",
+    "url": "/static/js/1.0499dbdb.chunk.js"
+  },
+  {
+    "revision": "e5efcf3fbff394eded74",
+    "url": "/static/css/main.1e1e9ad5.chunk.css"
+  },
+  {
+    "revision": "1d40f2305ff297331006d9163beb0bcc",
+    "url": "/index.html"
+  }
+];
