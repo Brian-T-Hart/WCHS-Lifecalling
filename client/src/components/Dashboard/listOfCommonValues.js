3295d2eaee7b8046108c3811fe7cff5444308140
Brian-T-Hart <hart.briant@gmail.com>
2022-11-19 12:48:09 -0800

    add array of commom valus

---
+++ client/src/components/Dashboard/listOfCommonValues.js
@@ -0,0 +1,194 @@
+const listOfCommonValues = [
+	{
+		id: 1,
+		name: "Acceptance",
+		description: "Having favor and approval from others"
+	},
+    {
+		id: 2,
+		name: "Achievement",
+		description: "Accomplishing something; especially with a high level of success or significant ability"
+	},
+    {
+		id: 3,
+		name: "Adventure",
+		description: "Participating in unusual or exciting experiences"
+	},
+    {
+		id: 4,
+		name: "Attractiveness",
+		description: "To draw others to you through pleasing behavior or appearance"
+	},
+    {
+		id: 5,
+		name: "Compassion",
+		description: "Awareness and heart-felt concern for another person's situation"
+	},
+    {
+		id: 6,
+		name: "Creativity",
+		description: "Quality of being original in thought, expression and imagination"
+	},
+    {
+		id: 7,
+		name: "Duty",
+		description: "Something that is expected or required of someone, often performed out of moral or legal obligation"
+	},
+    {
+		id: 8,
+		name: "Faithfulness",
+		description: "Remaining true to one's word, promises or vows"
+	},
+	{
+		id: 9,
+		name: "Fame",
+		description: "Widespread reputation and recognition among others"
+	},
+	{
+		id: 10,
+		name: "Fitness",
+		description: "Being in good physical condition and health"
+	},
+	{
+		id: 11,
+		name: "Freedom",
+		description: "State of having personal liberty, without confinement on life"
+	},
+	{
+		id: 12,
+		name: "Friendship",
+		description: "Affinity and closeness resulting from a mutual relationship"
+	},
+	{
+		id: 13,
+		name: "Fun",
+		description: "Enjoyment or playfulness"
+	},
+	{
+		id: 14,
+		name: "Generosity",
+		description: "Liberal in giving and sharing"
+	},
+	{
+		id: 15,
+		name: "Harmony",
+		description: "Seeking agreement and like-mindedness in relationships"
+	},
+	{
+		id: 16,
+		name: "Humility",
+		description: "Realistic opinion or estimate of one's own importance"
+	},
+	{
+		id: 17,
+		name: "Independence",
+		description: "Thinking or acting for oneself, free from the influence or control of others"
+	},
+	{
+		id: 18,
+		name: "Individuality",
+		description: "Qualities or characteristics that distinguish one from others"
+	},
+	{
+		id: 19,
+		name: "Integrity",
+		description: "Maintaining strict moral or ethical principals with honesty and genuineness"
+	},
+	{
+		id: 20,
+		name: "Justice",
+		description: "Upholding of what is morally right and fair"
+	},
+	{
+		id: 21,
+		name: "Kindness",
+		description: "Ability to treat others with love and care"
+	},
+	{
+		id: 22,
+		name: "Knowledge",
+		description: "Understanding fact, truth, or principles gained through experience or study"
+	},
+	{
+		id: 23,
+		name: "Loyalty",
+		description: "Maintaining one's allegiance or devotion to another"
+	},
+	{
+		id: 24,
+		name: "Moderation",
+		description: "Living a life free of excess or extremes"
+	},
+	{
+		id: 25,
+		name: "Order",
+		description: "A life where all aspects function correctly and there is little disruption or confusion"
+	},
+	{
+		id: 26,
+		name: "Popularity",
+		description: "Having favor from a certain group of people"
+	},
+	{
+		id: 27,
+		name: "Power",
+		description: "Capability to make things happen"
+	},
+	{
+		id: 28,
+		name: "Respect",
+		description: "To hold another person in esteem or honor"
+	},
+	{
+		id: 29,
+		name: "Responsibility",
+		description: "Being reliable and accountable for one's obligations and duties"
+	},
+	{
+		id: 30,
+		name: "Security",
+		description: "Freedom from care, anxiety or danger"
+	},
+	{
+		id: 31,
+		name: "Service",
+		description: "Taking actions to help others"
+	},
+	{
+		id: 32,
+		name: "Simplicity",
+		description: "A life that avoids complexity and luxury"
+	},
+	{
+		id: 33,
+		name: "Solitude",
+		description: "Time spent along in quiet peace"
+	},
+	{
+		id: 34,
+		name: "Tolerance",
+		description: "Accepting people different from yourself"
+	},
+	{
+		id: 35,
+		name: "Tradition",
+		description: "Handing down of cultural beliefs and practices within family and society"
+	},
+	{
+		id: 36,
+		name: "Comfort",
+		description: "A state of ease and satisfaction"
+	},
+	{
+		id: 37,
+		name: "Transparency",
+		description: "State of being open and straight forward"
+	},
+	{
+		id: 38,
+		name: "Wealth",
+		description: "Abundance of money, valuable possession, property or other riches"
+	}	
+]
+
+export default listOfCommonValues;
