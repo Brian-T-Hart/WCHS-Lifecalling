0378445e19fbc00d3eef7da4aa2dcc1772142603
Brian-T-Hart <hart.briant@gmail.com>
2022-11-22 11:31:22 -0800

    removed console log

--- client/src/components/MyCommonValues/MyCommonValues.js
+++ client/src/components/MyCommonValues/MyCommonValues.js
@@ -24,7 +24,6 @@ class MyCommonValues extends Component {
 	handleClick(e) {
 		e.preventDefault();
 		var commonValueName = e.target.dataset.txt;
-		console.log(commonValueName);
 		this.commonValueIndex(commonValueName);
 	}
 
