dd06b1633f72e94980c3b35a72af7fae72e4ed31
Brian-T-Hart <hart.briant@gmail.com>
2023-09-05 15:55:27 -0700

    replaced bar with an X marks the spot

--- client/src/components/MyMBTI/SliderBar.js
+++ client/src/components/MyMBTI/SliderBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 function SliderBar(props) {
-    const { barWidth } = props;
+    let { barWidth } = props;
     console.log(barWidth);
 
     if (barWidth === undefined) {
         return "";
     } else {
+        if (barWidth === "100") {
+            barWidth = "95";
+        }
+        
         return (
             <div className='slider-bar'>
-                <div style={{width: `${barWidth}%`}} className="slider-bar-inner"></div>
+                <div style={{left: `${barWidth}%`}} className="slider-bar-x">X</div>
+                {/* <div style={{width: `${barWidth}%`}} className="slider-bar-inner"></div> */}
                 <div className='slider-bar-labels'><span className="label-left">Introvert</span><span className="label-right">Extrovert</span></div>
             </div>
         )
