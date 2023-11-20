c424cec52790215b005c2c0f20b9f37086b62e64
Brian-T-Hart <hart.briant@gmail.com>
2023-11-19 16:43:32 -0800

    added sliderBarLabel1 and 2 to props

--- client/src/components/MyMBTI/SliderBar.js
+++ client/src/components/MyMBTI/SliderBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function SliderBar(props) {
-    let { barWidth } = props;
+    let { barWidth, slideBarLabel1, slideBarLabel2 } = props;
     console.log(barWidth);
 
     if (barWidth === undefined) {
@@ -14,8 +14,7 @@ function SliderBar(props) {
         return (
             <div className='slider-bar'>
                 <div style={{left: `${barWidth}%`}} className="slider-bar-x">X</div>
-                {/* <div style={{width: `${barWidth}%`}} className="slider-bar-inner"></div> */}
-                <div className='slider-bar-labels'><span className="label-left">Introvert</span><span className="label-right">Extrovert</span></div>
+                <div className='slider-bar-labels'><span className="label-left">{slideBarLabel1}</span><span className="label-right">{slideBarLabel2}</span></div>
             </div>
         )
     }
