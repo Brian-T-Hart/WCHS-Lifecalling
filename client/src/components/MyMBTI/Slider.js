4ed132b82a86503657cf32610c7c954a376acc06
Brian-T-Hart <hart.briant@gmail.com>
2023-11-19 16:44:08 -0800

    made slider more dynamic by adding more props

--- client/src/components/MyMBTI/Slider.js
+++ client/src/components/MyMBTI/Slider.js
@@ -1,34 +1,34 @@
 import React from 'react';
 
 function Slider(props) {
-    const { myMbti } = props;
+    const { sliderId, sliderLabel1, sliderLabel2, sliderValue, myMbti } = props;
 
-    if (myMbti === undefined) {
+    if (myMbti === undefined || sliderId === undefined) {
         return "";
     } else {
-        const sliderValue = myMbti[4] === undefined ? "0" : myMbti[4];
+        const defaultValue = sliderValue === undefined ? "0" : sliderValue;
 
         return (
             <div className="form-group">
-                <label htmlFor="mbti-slider">Where are you on the Introvert/Extrovert Scale?</label>
+                <label htmlFor={sliderId}>{"Where are you on the " + sliderId + " scale?"}</label>
                 <input
                     type="range"
-                    defaultValue={sliderValue}
+                    defaultValue={defaultValue}
                     className="slider"
-                    id="mbti-slider"
-                    list="mbti-markers"
+                    id={sliderId}
+                    list={sliderId + "-markers"}
                     min="0"
                     max="100"
                     name="mbti-slider"
                     step="20"
                 />
-                <datalist className="markers" id="mbti-markers">
-                    <option value="0" label="Introvert"></option>
+                <datalist className="markers" id={sliderId + "-markers"}>
+                    <option value="0" label={sliderLabel1}></option>
                     <option value="20" label=""></option>
                     <option value="40" label=""></option>
                     <option value="60" label=""></option>
                     <option value="80" label=""></option>
-                    <option value="100" label="Extrovert"></option>
+                    <option value="100" label={sliderLabel2}></option>
                 </datalist>
             </div>
         )
